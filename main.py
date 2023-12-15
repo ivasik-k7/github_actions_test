0cf40529a4491d7d5b765cfd8ca1ede4878738c2
Ivan Kovtun <ivan.kovtun@capgemini.com>
2023-12-15 15:45:43 +0100

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    elements: list = [
+        "word",
+        "blyoasd",
+        "sabaka",
+        "teststesttsdsjds" "teststesttsdsjds" "testsslldsopdps" "teststedsdsd00322s",
+    ]
+    eliment: str = "cyfra drwa"
+    print("Test")
